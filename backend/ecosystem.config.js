d19c874aa5e06a85966c0d4a6af4c41fff0cbeae
lang tu van <langtuvan@gmail.com>
2026-01-26 20:23:23 +0700

    night 26

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'order-backend',
-      script: './dist/main.js',
+      script: './dist/src/main.js',
       instances: 'max',
       exec_mode: 'cluster',
       autorestart: true,
@@ -10,7 +10,7 @@ module.exports = {
       max_memory_restart: '1G',
       env_production: {
         NODE_ENV: 'production',
-        PORT: 5001 || 5000,
+        PORT: process.env.APP_PORT || 5000,
       },
       error_file: './logs/err.log',
       out_file: './logs/out.log',
